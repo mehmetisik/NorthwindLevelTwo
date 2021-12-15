ec691a496aa5e4bddef3aeeb1966ede9f7a03139
Mehmet IŞIK <mehmetISIK1989@hotmail.com>
2021-12-15 23:22:38 +0300

    Entity Katmanında Repository İmplementasyonunu Yapmak

---
+++ Entities/Dtos/ProductDetail.cs
@@ -0,0 +1,23 @@
+﻿using Core.Entites.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Dtos
+{
+    public class ProductDetail : IDto
+    {
+
+        public int ProductId { get; set; }
+
+        public string ProductName { get; set; }
+
+        public decimal UnitPrice { get; set; }
+
+        public string CategoryName { get; set; }
+
+
+    }
+}
