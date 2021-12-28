67b70af06db7e84c419f6dec7add8a0de124902a
Mehmet IŞIK <mehmetISIK1989@hotmail.com>
2021-12-29 00:10:45 +0300

    UI geçmeden önce

--- DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
+++ DataAccess/Concrete/EntityFramework/EfCategoryDal.cs
@@ -1,5 +1,6 @@
 ﻿using Core.DataAccess.Concrete.EntityFramework;
 using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
--- DataAccess/Concrete/EntityFramework/EfProductDal.cs
+++ DataAccess/Concrete/EntityFramework/EfProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using Core.DataAccess.Concrete.EntityFramework;
 using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework.Contexts;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
@@ -10,7 +11,7 @@ using System.Threading.Tasks;
 
 namespace DataAccess.Concrete.EntityFramework
 {
-    public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext> IProductDal
+    public class EfProductDal : EfEntityRepositoryBase<Product,NorthwindContext>, IProductDal
     {
        
     }
